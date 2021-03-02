2815b1d0a1906689cace976de17b5a89f25770b4
Nata Relva <natan.danilo@gmail.com>
2021-03-02 18:54:07 -0300

    ESTRUTURA DE PASTAS CRIADA

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+  /*
+    O que significa: express.urlencoded({ extended: true }) ?
+
+    A opção "extended" diz para o express qual
+    biblioteca ele deve utilizar para fazer o
+    parsing do conteúdo das requisições que ele recebe.
+    Quando extended : true vai utilizar a biblioteca qs
+    e quando for false ele vai utilizar a biblioteca querystring.
+
+    O que significa: this.app.use(express.json());
+
+    Configuração para app.user também entender json
+  */
+
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
+/*
+  Exportando apenas o app dentro da classa App, sendo ele o express que precisamos.
+*/
